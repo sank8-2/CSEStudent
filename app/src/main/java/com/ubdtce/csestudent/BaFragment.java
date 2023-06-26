fa945090d18909828bdfbc9c2ef4c05d0d567290
Sanketh Kumar <sankethkumarbg1995@gmail.com>
2023-06-26 13:26:34 +0530

    Update

--- app/src/main/java/com/ubdtce/csestudent/BaFragment.java
+++ app/src/main/java/com/ubdtce/csestudent/BaFragment.java
@@ -1,5 +1,7 @@
 package com.ubdtce.csestudent;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,58 +9,19 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link BaFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class BaFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+public class BaFragment extends Fragment{
     public BaFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment BaFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static BaFragment newInstance(String param1, String param2) {
-        BaFragment fragment = new BaFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_ba, container, false);
+
     }
 }
--- app/src/main/java/com/ubdtce/csestudent/Navigation.java
+++ app/src/main/java/com/ubdtce/csestudent/Navigation.java
@@ -10,8 +10,12 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.navigation.NavigationView;
 
@@ -36,6 +40,7 @@ public class Navigation extends AppCompatActivity {
 
         loadFragment(new AFragment());
 
+
         navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -44,16 +49,16 @@ public class Navigation extends AppCompatActivity {
                 if(id==R.id.optHome){
                     loadFragment(new AFragment());
                 } else if (id==R.id.optProfile) {
-
+                    loadFragment(new BaFragment());
                 }
                  else if (id==R.id.optFaculty) {
-
+                    loadFragment(new CFragment());
                 }
                  else if (id==R.id.optCgpa) {
-
+                    loadFragment(new DFragment());
                 }
                  else{
-
+                    loadFragment(new EFragment());
                 }
 
                  navigationBar.closeDrawer(GravityCompat.START);
@@ -63,8 +68,9 @@ public class Navigation extends AppCompatActivity {
             }
         });
 
-    }
 
+
+    }
     @Override
     public void onBackPressed(){
         if(navigationBar.isDrawerOpen(GravityCompat.START)){
