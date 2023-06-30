c123c269a94220dd3a13c7161e24c9971fde2820
shraddhaM02 <109832583+shraddhaM02@users.noreply.github.com>
2023-06-30 09:13:07 +0530

    updated fragment

--- app/src/main/java/com/ubdtce/csestudent/DbTimetable.java
+++ app/src/main/java/com/ubdtce/csestudent/DbTimetable.java
@@ -11,13 +11,13 @@ public class DbTimetable extends SQLiteOpenHelper {
     private static final String DB_NAME = "TIMETABLE DB";
     private static final String TBL_NAME = "TIMETABLE";
     private static final String KEY_DAYS = "DAYS";
-    private static final String KEY_ONE = "1";
-    private static final String KEY_TWO= "2";
-    private static final String KEY_THREE = "3";
-    private static final String KEY_FOUR = "4";
-    private static final String KEY_FIVE = "5";
-    private static final String KEY_SIX = "6";
-    private static final String KEY_SEVEN = "7";
+    private static final String KEY_ONE = "one";
+    private static final String KEY_TWO= "two";
+    private static final String KEY_THREE = "three";
+    private static final String KEY_FOUR = "four";
+    private static final String KEY_FIVE = "five";
+    private static final String KEY_SIX = "six";
+    private static final String KEY_SEVEN = "seven";
     private static final int DB_VERSION = 1;
 
     public DbTimetable(@Nullable Context context) {
@@ -26,15 +26,15 @@ public class DbTimetable extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-//        db.execSQL("CREATE TABLE "+TBL_NAME
-//                +"("+KEY_DAYS+" TEXT PRIMARY KEY,"
-//                +KEY_ONE+"TEXT,"
-//                +KEY_TWO+"TEXT,"
-//                +KEY_THREE+"TEXT,"
-//                +KEY_FOUR+"TEXT,"
-//                +KEY_FIVE+"TEXT,"
-//                +KEY_SIX+"TEXT,"
-//                +KEY_SEVEN+"TEXT)");
+        db.execSQL("CREATE TABLE "+TBL_NAME
+                +"("+KEY_DAYS+" TEXT PRIMARY KEY,"
+                +KEY_ONE+"TEXT,"
+                +KEY_TWO+"TEXT,"
+                +KEY_THREE+"TEXT,"
+                +KEY_FOUR+"TEXT,"
+                +KEY_FIVE+"TEXT,"
+                +KEY_SIX+"TEXT,"
+                +KEY_SEVEN+"TEXT)");
     }
 
     @Override
