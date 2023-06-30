fbed6b657baaeeebcd3ca28f182de4ed4d8f208c
shraddhaM02 <109832583+shraddhaM02@users.noreply.github.com>
2023-06-30 13:30:26 +0530

    profile edited

--- app/src/main/java/com/ubdtce/csestudent/AFragment.java
+++ app/src/main/java/com/ubdtce/csestudent/AFragment.java
@@ -3,14 +3,23 @@ package com.ubdtce.csestudent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AFragment extends Fragment {
 
+
+    ViewPager viewPager;
     public AFragment() {
+
         // Required empty public constructor
     }
 
@@ -19,6 +28,19 @@ public class AFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
+        View inflateIt = inflater.inflate(R.layout.fragment_a, container, false);
+
+        viewPager = (ViewPager) inflateIt.findViewById(R.id.viewPager);
+        List<Integer> imageList = new ArrayList<>();
+
+        imageList.add(R.drawable.focs);
+        imageList.add(R.drawable.sen);
+        imageList.add(R.drawable.sen1);
+        imageList.add(R.drawable.clg);
+        MyAdapter MyAdapter = new MyAdapter(imageList);
+        viewPager.setAdapter(MyAdapter);
+
         return inflater.inflate(R.layout.fragment_a, container, false);
     }
 }
---
+++ app/src/main/java/com/ubdtce/csestudent/MyAdapter.java
@@ -0,0 +1,48 @@
+package com.ubdtce.csestudent;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+import java.util.List;
+
+public class MyAdapter extends PagerAdapter {
+
+    List<Integer> list;
+
+    MyAdapter(List<Integer> imgList){
+        this.list=imgList;
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view==object;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        View view= LayoutInflater.from(container.getContext()).inflate(R.layout.image_layout,container,false);
+        final ImageView imageView=view.findViewById(R.id.imageView);
+
+
+
+        imageView.setImageResource(list.get(position));
+        container.addView(view);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+       container.removeView((View) object);
+    }
+}
