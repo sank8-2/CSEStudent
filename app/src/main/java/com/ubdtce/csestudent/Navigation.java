b01b0e470e9d7f1f4042100a271ffd9d33d6bc34
Sanketh Kumar <sankethkumarbg1995@gmail.com>
2023-07-02 20:06:39 +0530

    Made Faculty a bit More Wow

--- app/src/main/java/com/ubdtce/csestudent/Navigation.java
+++ app/src/main/java/com/ubdtce/csestudent/Navigation.java
@@ -6,10 +6,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.app.Dialog;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -18,6 +20,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.android.material.navigation.NavigationView;
@@ -200,4 +204,5 @@ public class Navigation extends AppCompatActivity {
     public void goToProfile() {
         loadFragment(new BaFragment());
     }
+
 }
